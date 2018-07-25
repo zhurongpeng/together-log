44f6d93eb41441ad8978eda510441c9616045c0a
zhurongpeng <zhurongpeng@heifan.cn>
2018-07-25 13:57:45 +0800

    fix

--- src/PLog.php
+++ src/PLog.php
@@ -1,10 +1,10 @@
 <?php
-namespace Zrp;
+namespace Together;
 
 use Monolog\Logger;
 use Monolog\Handler\SyslogHandler;
 
-class PLog
+class Log
 {
     protected $logger;
     protected $appName;
@@ -20,30 +20,82 @@ class PLog
         $this->logger->pushHandler(new SyslogHandler($this->appName.'/'.$tag, 'local7', 100));
     }
 
-    public function info($tag, $log)
-    {
-        $this->handle($tag);
+    // public function info($tag, $log)
+    // {
+    //     $this->handle($tag);
+    //
+    //     $this->logger->addInfo($log);
+    // }
 
-        $this->logger->addInfo($log);
+    public static function getLogger()
+    {
+        return self::$logger ?: self::$logger = self::createDefaultLogger();
     }
 
-    public function warn()
+    /**
+     * Set logger.
+     *
+     * @param LoggerInterface $logger
+     */
+    public static function setLogger(LoggerInterface $logger)
     {
-
+        self::$logger = $logger;
     }
 
-    public function error()
+    /**
+     * Tests if logger exists.
+     *
+     * @return bool
+     */
+    public static function hasLogger(): bool
     {
-
+        return self::$logger ? true : false;
     }
 
-    public function notice()
+    /**
+     * Make a default log instance.
+     *
+     * @return \Monolog\Logger
+     */
+    protected static function createDefaultLogger()
     {
+        $handler = new RotatingFileHandler(getenv('LOGGER_PATH'));
+        $handler->setFormatter(new LineFormatter());
 
+        $logger = new Logger(getenv('APP_NAME'));
+        $logger->pushHandler($handler);
+
+        return $logger;
     }
 
-    public function fatal()
+    /**
+     * Forward call.
+     *
+     * @param string $method
+     * @param array  $args
+     *
+     * @return mixed
+     */
+    public static function __callStatic($method, $args)
     {
+        return forward_static_call_array([self::getLogger(), $method], $args);
+    }
 
+    /**
+     * Forward call.
+     *
+     * @param string $method
+     * @param array  $args
+     *
+     * @return mixed
+     */
+    public function __call($method, $args)
+    {
+        $this->handle($args[0]);
+
+        unset($args[0]);
+
+        return call_user_func_array([self::getLogger(), $method], $args);
     }
 }
+
--- src/test.php
+++ src/test.php
@@ -2,7 +2,13 @@
 
 require 'vendor/autoload.php';
 
-$log = new Monolog\Logger('testname');
-$log->pushHandler(new Monolog\Handler\StreamHandler('/home/zhurongpeng/workspace/test/logs/test.log', Monolog\Logger::WARNING));
+// $log = new Monolog\Logger('testname');
+// $log->pushHandler(new Monolog\Handler\StreamHandler('/home/zhurongpeng/workspace/test/logs/test.log', Monolog\Logger::WARNING));
+//
+// $log->info(json_encode(['test', '测试']));
 
-$log->addWarning('测试');
+use Zrp\PLog;
+
+$log = new PLog('test');
+
+$log->info('test', 'ceshi');
